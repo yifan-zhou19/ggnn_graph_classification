fix the class prediction dimension in softmax layer

main.py
@@ -16,7 +16,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--task_id', type=int, default=4, help='bAbI task id')
 parser.add_argument('--question_id', type=int, default=0, help='question types')
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
-parser.add_argument('--batchSize', type=int, default=1, help='input batch size')
+parser.add_argument('--batchSize', type=int, default=32, help='input batch size')
 parser.add_argument('--state_dim', type=int, default=10, help='GGNN hidden state size')
 parser.add_argument('--n_steps', type=int, default=5, help='propogation steps number of GGNN')
 parser.add_argument('--niter', type=int, default=150, help='number of epochs to train for')
@@ -35,7 +35,6 @@ random.seed(opt.manualSeed)
 torch.manual_seed(opt.manualSeed)
 
 opt.dataroot = 'babi_data/processed_1/train/%d_graphs.txt' % opt.task_id
-
 opt.directory = "program_data/babi_format"
 
 if opt.cuda:
@@ -73,11 +72,13 @@ def main(opt):
 
 # This part is the implementation to illustrate Graph-Level output from program data
 def main2(opt):
-    train_dataset = bAbIDataset2(opt.directory, True)
+    opt.n_classes = 20
+
+    train_dataset = bAbIDataset2(opt.directory, True, opt.n_classes)
     train_dataloader = bAbIDataloader(train_dataset, batch_size=opt.batchSize, \
                                       shuffle=True, num_workers=0)
 
-    test_dataset = bAbIDataset2(opt.directory, False)
+    test_dataset = bAbIDataset2(opt.directory, False, opt.n_classes)
     test_dataloader = bAbIDataloader(test_dataset, batch_size=opt.batchSize, \
                                      shuffle=True, num_workers=2)
 

model.py
@@ -74,6 +74,7 @@ class GGNN(nn.Module):
         self.n_edge_types = opt.n_edge_types
         self.n_node = opt.n_node
         self.n_steps = opt.n_steps
+        self.n_classes = opt.n_classes
 
         for i in range(self.n_edge_types):
             # incoming and outgoing edge embedding
@@ -93,14 +94,22 @@ class GGNN(nn.Module):
             nn.Linear(self.state_dim, self.state_dim),
             nn.Tanh(),
             nn.Linear(self.state_dim, 1),
-            nn.Tanh()
+            nn.Tanh(),   
+        )
+
+        self.class_prediction = nn.Sequential(
+            nn.Linear(self.n_node, 100),
+            nn.Tanh(),
+            nn.Linear(100, self.n_classes),
+            nn.Softmax()    
         )
 
         self.soft_attention = nn.Sequential(
             nn.Linear(self.state_dim, self.state_dim),
             nn.Tanh(),
             nn.Linear(self.state_dim, 1),
-            nn.Sigmoid()
+            nn.Sigmoid(),
+           
         )
 
 
@@ -141,10 +150,16 @@ class GGNN(nn.Module):
         soft_attention_ouput = self.soft_attention(prop_state)
 
         output = output + soft_attention_ouput
+
+    
         # print("&&&&&")
         # print(output)
         # print(output.shape)
         output = output.sum(2)
+        # print("JAPS{DJPS")
+        # print(output.shape)
+        output = self.class_prediction(output)
+        # print(output)
         # print("#####")
         # print(output)
         # print(output.shape)

utils/data/dataset.py
@@ -28,12 +28,12 @@ def load_graphs_from_file(file_name):
     # print(data_list)
     return data_list
 
-def load_program_graphs_from_directory(directory,is_train=True):
+def load_program_graphs_from_directory(directory,is_train=True,n_classes=3):
     data_list = []
     edge_list = []
     target_list = []
     
-    for i in range(1,3):
+    for i in range(1,n_classes):
 
         if is_train == True:
             path =  os.path.join(directory,"train", "train_" + str(i) + ".txt")
@@ -126,7 +126,6 @@ def data_convert(data_list, n_annotation_dim):
         edge_list = item[0]
         target_list = item[1]
         for target in target_list:
-            print(target_list)
             task_type = target[0]
             task_output = target[-1]
             annotation = np.zeros([n_nodes, n_annotation_dim])
@@ -145,8 +144,12 @@ def data_convert_for_program_data(data_list, n_annotation_dim):
         for target in target_list:
             task_type = target[0]
             task_output = target[-1]
-            annotation = np.zeros([n_nodes, n_annotation_dim])
-            # annotation[target[1]-1][0] = 0
+            annotation = np.zeros([n_nodes, n_annotation_dim])   
+            for edge in edge_list:
+                src_idx = edge[0]
+                # print(src_idx)
+                annotation[src_idx-1][0] = 1
+          
             task_data_list.append([edge_list, annotation, task_output])
     return task_data_list
 
@@ -196,7 +199,7 @@ class bAbIDataset():
             self.data = all_task_val_data[question_types]
 
     def __getitem__(self, index):
-        print(self.data[index])
+        # print(self.data[index])
         
         am = create_adjacency_matrix(self.data[index][0], self.n_node, self.n_edge_types)
         annotation = self.data[index][1]
@@ -208,9 +211,9 @@ class bAbIDataset():
 
 class bAbIDataset2():
    
-    def __init__(self, path, is_train):
+    def __init__(self, path, is_train, n_classes):
        
-        all_data = load_program_graphs_from_directory(path,is_train)
+        all_data = load_program_graphs_from_directory(path,is_train,n_classes)
         all_data = np.array(all_data)[0:len(all_data)]
        
         if is_train == True:
@@ -218,7 +221,7 @@ class bAbIDataset2():
         else:
             print("Number of all testing data : " + str(len(all_data)))
         self.n_edge_types =  find_max_edge_id(all_data)
-        print("Edge types : " + str(self.n_edge_types))
+        # print("Edge types : " + str(self.n_edge_types))
         self.n_tasks = find_max_task_id(all_data)
         # self.n_node = find_max_node_id(all_data)
         self.n_node = 48