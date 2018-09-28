bfc70a7adc1b89a04c207e4ad19c11874a2cb49a
Yijun Yu <y.yu@open.ac.uk>
2018-09-28 15:38:56 +0100

    using local_maps instead of the global maps

--- program_data/generate_cpp_babi_format.sh
+++ program_data/generate_cpp_babi_format.sh
@@ -1,19 +1,14 @@
-mkdir -p cpp_babi_format/train cpp_babi_format/test
-mkdir -p cpp_protobuf_format
-<<<<<<< HEAD
-rm -f maps.cpp.pkl
-for index in {1..104}; do
-    if [ ! -f cpp_protobuf_format/$index.fbs ]; then
-	    fast -S -G CppProgramData/$index cpp_protobuf_format/$index.fbs
-    fi
-    ggnn cpp_protobuf_format/$index.fbs cpp_babi_format/train/train_$index.txt cpp_babi_format/test/test_$index.txt
-=======
-chmod a+wx cpp_protobuf_format cpp_babi_format/train cpp_babi_format/test
+input=CppProgramData
+in=cpp_protobuf_format
+out=cpp_babi_format_10
+
+mkdir -p $out/train $out/test
+mkdir -p $in
+chmod a+wx $in $out/train $out/test
 rm -f maps.cpp.pkl
 for index in {1..10}; do
-#    if [ ! -f cpp_protobuf_format/$index.fbs ]; then
-#	    fast -S -G CppProgramData/$index cpp_protobuf_format/$index.fbs
-#    fi
-    ggnn cpp_protobuf_format/$index.fbs cpp_babi_format/train/train_$index.txt cpp_babi_format/test/test_$index.txt # > $index.log
->>>>>>> 8194f5a58412850b059d846f82c9e1cbfe931118
+    if [ ! -f $in/$index.fbs ]; then
+	    fast -S -G $input/$index $in/$index.fbs
+    fi
+    ggnn $in/$index.fbs $out/train/train_$index.txt $out/test/test_$index.txt # > $index.log
 done
