5d6b41d3a7b99bc787d96f0ee8754fc1cf4c2b46
Yijun Yu <y.yu@open.ac.uk>
2018-09-28 09:58:15 +0100

    updated ggnn.py for patching

--- program_data/generate_cpp_babi_format.sh
+++ program_data/generate_cpp_babi_format.sh
@@ -1,9 +1,9 @@
 mkdir -p cpp_babi_format/train cpp_babi_format/test
 mkdir -p cpp_protobuf_format
 chmod a+wx cpp_protobuf_format cpp_babi_format/train cpp_babi_format/test
-for index in {1..104}; do
+for index in {1..10}; do
     if [ ! -f cpp_protobuf_format/$index.fbs ]; then
-	    fast:v0.0.8-1 -S -G CppProgramData/$index cpp_protobuf_format/$index.fbs
+	    fast -S -G CppProgramData/$index cpp_protobuf_format/$index.fbs
     fi
-    ggnn cpp_protobuf_format/$index.fbs cpp_babi_format/train/train_$index.txt cpp_babi_format/test/test_$index.txt
+    ggnn cpp_protobuf_format/$index.fbs cpp_babi_format/train/train_$index.txt cpp_babi_format/test/test_$index.txt > $index.log
 done
