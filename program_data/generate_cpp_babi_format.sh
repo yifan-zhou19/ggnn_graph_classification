bde1faaaec94a0b5622e5942e3f2c815749b9bbb
Yijun Yu <y.yu@open.ac.uk>
2018-09-30 09:55:33 +0100

    exploratory experiments

--- program_data/generate_cpp_babi_format.sh
+++ program_data/generate_cpp_babi_format.sh
@@ -3,13 +3,15 @@ in=${1:-cpp_protobuf_format_correct}
 out=${in/protobuf/babi}$2
 mkdir -p $out/train $out/test
 mkdir -p $in
-chmod -R a+w $in $out
 rm -f maps.cpp.pkl
 rm -f maps.pkl
-for index in {1..5}; do
+for index in {1..3}; do
     if [ ! -f $in/$index.fbs ]; then
-	    fast -S -G $input/$index $in/$index.fbs
+	chmod -R a+w $in
+	fast > $in/fast.version
+	fast -S -G $input/$index $in/$index.fbs
     fi
+    chmod -R a+w $out
     ggnn $in/$index.fbs $out/train/train_$index.txt $out/test/test_$index.txt > $out/$index.log
     ggnn $in/$index.fbs $out/$index.json >> $out/$index.log
 done
