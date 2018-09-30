43dd0c2b3cc5a8f78a54ef3ae576e9bfaae019c0
Yijun Yu <y.yu@open.ac.uk>
2018-09-30 08:21:18 +0100

    exploratory experiments

--- program_data/generate_cpp_babi_format.sh
+++ program_data/generate_cpp_babi_format.sh
@@ -1,13 +1,18 @@
 input=CppProgramData
-in=cpp_protobuf_format_ten
-out=cpp_babi_format_ten
+in=${1:-cpp_protobuf_format_correct}
+out=${in/protobuf/babi}$2
 mkdir -p $out/train $out/test
 mkdir -p $in
 chmod a+wx $in $out/train $out/test
 rm -f maps.cpp.pkl
+rm -f maps.pkl
 for index in {1..10}; do
     if [ ! -f $in/$index.fbs ]; then
 	    fast -S -G $input/$index $in/$index.fbs
     fi
     ggnn $in/$index.fbs $out/train/train_$index.txt $out/test/test_$index.txt # > $index.log
 done
+mv maps.cpp.pkl $out
+mv maps.pkl $out
+docker run -v $(pwd)/$out:/e --entrypoint bash -it ggnn -c "cp /usr/local/bin/ggnn /e/ggnn.py"
+cp $0 $out
