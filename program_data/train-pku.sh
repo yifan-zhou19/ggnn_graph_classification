5513290f88a740a30856574b62a5c8a46d445ca0
Yijun Yu <y.yu@open.ac.uk>
2018-10-18 22:47:14 +0100

    update pku specific scripts

--- program_data/train-pku.sh
+++ program_data/train-pku.sh
@@ -1,9 +1,10 @@
 lang=${1:-cpp}
 MEMORY=256G
-BATCH_SIZE=128
+TRAIN_SIZE=32
 in="$lang"_protobuf_format_Oct-15-2018
 out=${in/protobuf/babi}-00000${2:-23}
 n=${3:-104}
 cd .. > /dev/null
-run program_data/$out $n 300 8
+	TRAIN_SIZE=32 \
+	run program_data/$out $n 300 8
 cd - > /dev/null
