d29aaa8a6806b1e82b63d24c4e33205881c8dbb8
Yijun Yu <y.yu@open.ac.uk>
2018-10-19 16:35:47 +0100

    fixing test script for github datasets 4:1

--- program_data/train.sh
+++ program_data/train.sh
@@ -1,12 +1,41 @@
+function size_voc() {
+	cd $1 > /dev/null
+        lang=$(ls maps.*.pkl)
+        lang=${lang/maps./}
+        lang=${lang/.pkl/}
+	k=$(docker run -v $(pwd):/e --entrypoint /opt/bin/maps -it yijun/fast:built --lang $lang | sort -n | wc -l)
+	cd - > /dev/null
+	echo $((k-1))
+}
 lang=${1:-cpp}
-MEMORY=256G
-BATCH_SIZE=128
-in=github_"$lang"_protobuf_format_Oct-15-2018
-out=${in/protobuf/babi}-00000${2:-24}
-n=${3:-50}
-cd $out > /dev/null
-k=$(docker run -v $(pwd):/e --entrypoint /opt/bin/maps -it yijun/fast:built --lang $lang | sort -n | wc -l)
-cd - > /dev/null
-cd .. > /dev/null
-run program_data/$out $((k-1)) $n 300 8
-cd - > /dev/null
+in=github_"$lang"_protobuf_format_Oct-10-2018-0000028
+lang1=${in/protobuf/babi}
+n=${2:-104}
+k=$(size_voc $lang1)
+docker build -t progress ../progress
+log=$lang1/log-$n.txt
+mkdir -p model
+if [ ! -f $log ]; then
+ mkdir -p $(dirname $log)
+ mkdir -p $(dirname $log)/logs
+ chmod o+w $(dirname $log)/logs
+ touch -f $log
+fi
+        NV_GPU=1 \
+  /usr/bin/time -f %e \
+  nvidia-docker run -v $(dirname $(pwd)):/e -w /e --shm-size 11G --rm -it progress \
+  python main_ggnn.py \
+	--cuda \
+	--training \
+	--n_classes $n \
+        --directory  program_data/$lang1 \
+	--model_path program_data/$lang1/$n.cpkl \
+	--log_path program_data/$lang1/logs \
+        --state_dim 5 \
+	--n_steps 5 \
+	--n_hidden 50 \
+	--niter 200 \
+	--size_vocabulary $k \
+	--train_batch_size 32 \
+	--test_batch_size 32 \
+  | tee -a $log
