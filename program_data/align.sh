a7f18f7f9d57715c1867b22874a917734b1cc4c7
Yijun Yu <y.yu@open.ac.uk>
2018-10-19 16:46:32 +0100

    simplify align docker image

--- program_data/align.sh
+++ program_data/align.sh
@@ -1,2 +1,2 @@
-docker build --build-arg http_proxy=http://wwwcache.open.ac.uk:80 --build-arg https_proxy=http://wwwcache.open.ac.uk:80 -t align align > align.log
-docker run -v $(pwd):/e -w /e -it align "$@"
+docker build --build-arg http_proxy=http://wwwcache.open.ac.uk:80 --build-arg https_proxy=http://wwwcache.open.ac.uk:80 -t ggnn ggnn > ggnn.log
+docker run -v $(pwd):/e -w /e -it ggnn "$@"
