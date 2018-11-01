e981b736001e13dc81c81ad1062bcb541abae6da
Yijun Yu <y.yu@open.ac.uk>
2018-11-01 15:09:55 +0000

    the encoding across equivalent classes shall be different

--- program_data/meaning.sh
+++ program_data/meaning.sh
@@ -5,15 +5,17 @@ function fast2ggnn() {
 	docker run --rm -e http_proxy=http://wwwcache.open.ac.uk:80 -e https_proxy=http://wwwcache.open.ac.uk:80 --rm -v $(pwd):/e --entrypoint /usr/local/bin/fast2ggnn -t ggnn $@
 }
 find meaningful/src -type f | while read src; do
-  tgt=${src/src/tgt}.json
+  tgt=${src/src/tgt}.txt
   if [ ! -f $tgt ]; then
 	  mkdir -p $(dirname $tgt)
 	  chmod a+w $(dirname $tgt)
 	  fast2ggnn $src $tgt
   fi
 done
-for f in meaningful/tgt/*; do
-  if [ -d $f ]; then
+ls -d meaningful/tgt/* > /tmp/folders.txt
+i=0
+cat /tmp/folders.txt | while read f; do
+  i=$((i+1))
     n=$(ls $f/* | wc -l | cut -f1 -d" ")
     if [[ $n > 1 ]]; then
        f1=$(ls $f/* | head -1)
@@ -23,5 +25,18 @@ for f in meaningful/tgt/*; do
           fi
        done
     fi
-  fi 
+    j=0
+    cat /tmp/folders.txt | while read g; do
+       j=$((j+1))
+       if [[ $i < $j ]]; then
+         ls $f/* | while read u; do
+          ls $g/* | while read v; do
+	    d=$(diff $u $v| wc -l | cut -f1 -d" ")
+            if [[ $d = 0 ]]; then
+		echo $u $v
+            fi
+          done
+         done
+       fi
+    done
 done
--- program_data/meaningful/src/1/4.java
+++ program_data/meaningful/src/1/4.java
@@ -1,7 +1,7 @@
    for (int k=0; k<n-1; k++)
       for (int j=k+1; j<n; j++)
-          if (data[k] >= data[j]) {
-             int tmp = data[k];
+          if (data[k] < data[j]) {
+             float tmp = data[k];
              data[k] = data[j];
              data[j] = tmp;
           }
