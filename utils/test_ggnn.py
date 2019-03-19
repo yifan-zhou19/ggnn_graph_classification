d7e1cd9fc1bb010ec6574bbdb45d2c0123e332b4
Quoc Nghi <bdqnghi@gmail.com>
2019-03-19 18:53:42 +0800

    update

--- utils/test_ggnn.py
+++ utils/test_ggnn.py
@@ -10,11 +10,12 @@ def test(dataloader, net, criterion, optimizer, opt):
     all_targets = []
     all_predicted = []
 
-    for i, (adj_matrix, target) in enumerate(dataloader, 0):
+    for i, (adj_matrix, embedding_matrix, target) in enumerate(dataloader, 0):
         
         # padding = torch.zeros(len(annotation), opt.n_node, opt.state_dim - opt.annotation_dim).double()
         # init_input = torch.cat((annotation, padding), 2)
-        init_input = torch.zeros(len(adj_matrix), opt.n_node, opt.state_dim).double()
+        # init_input = torch.zeros(len(adj_matrix), opt.n_node, opt.state_dim).double()
+        init_input = embedding_matrix
         if opt.cuda:
             init_input = init_input.cuda()
             adj_matrix = adj_matrix.cuda()
